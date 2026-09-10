2faf906491322ed5b284e5ed4d1b1a3d2c72cdaa
JYFFF96 <59434545+JYFFF96@users.noreply.github.com>
2026-09-10 23:20:43 +0800

    V1.0-RC1 add mini program charge and transaction APIs

--- miniprogram/utils/api.js
+++ miniprogram/utils/api.js
@@ -7,25 +7,21 @@ function request(path, options = {}) {
       method: options.method || 'GET',
       data: options.data || {},
       timeout: 5000,
+      header: {'content-type':'application/json'},
       success: res => {
         if (res.statusCode >= 200 && res.statusCode < 300) resolve(res.data)
-        else reject(new Error(`HTTP ${res.statusCode}`))
+        else reject(new Error((res.data && res.data.detail) || `HTTP ${res.statusCode}`))
       },
       fail: reject
     })
   })
 }
 
-function getDevices() {
-  return request('/api/devices')
-}
+const getDevices = () => request('/api/devices')
+const getRealtime = pileCode => request(`/api/devices/${pileCode}/realtime`)
+const startCharge = data => request('/api/charge/start',{method:'POST',data})
+const stopCharge = data => request('/api/charge/stop',{method:'POST',data})
+const getTransactions = () => request('/api/transactions')
+const getTariff = () => request('/api/tariff')
 
-function getRealtime(pileCode) {
-  return request(`/api/devices/${pileCode}/realtime`)
-}
-
-module.exports = {
-  request,
-  getDevices,
-  getRealtime
-}
+module.exports = {request,getDevices,getRealtime,startCharge,stopCharge,getTransactions,getTariff}
