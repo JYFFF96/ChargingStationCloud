aad40e5490dad13bfa6f992758c23d1d2c057b4a
JYFFF96 <59434545+JYFFF96@users.noreply.github.com>
2026-09-10 22:59:21 +0800

    V0.2 update mini program home data model

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,29 +1,18 @@
 const api = require('../../utils/api')
 
 Page({
-  data: {
-    loading: false,
-    error: '',
-    devices: []
+  data: { loading:false, error:'', devices:[], onlineCount:0, chargingCount:0, faultCount:0 },
+  onLoad(){ this.loadDevices() },
+  onShow(){ this.loadDevices() },
+  onPullDownRefresh(){ this.loadDevices().finally(()=>wx.stopPullDownRefresh()) },
+  async loadDevices(){
+    this.setData({loading:true,error:''})
+    try{
+      const data=await api.getDevices(), devices=data.items||[]
+      this.setData({devices,onlineCount:devices.filter(x=>x.online).length,chargingCount:0,faultCount:0})
+    }catch(err){this.setData({error:err.errMsg||err.message||'请求失败'})}
+    finally{this.setData({loading:false})}
   },
-
-  onLoad() {
-    this.loadDevices()
-  },
-
-  onPullDownRefresh() {
-    this.loadDevices().finally(() => wx.stopPullDownRefresh())
-  },
-
-  async loadDevices() {
-    this.setData({ loading: true, error: '' })
-    try {
-      const data = await api.getDevices()
-      this.setData({ devices: data.items || [] })
-    } catch (err) {
-      this.setData({ error: err.errMsg || err.message || '请求失败' })
-    } finally {
-      this.setData({ loading: false })
-    }
-  }
+  goScan(){ wx.switchTab({url:'/pages/scan/scan'}) },
+  goCharging(){ wx.switchTab({url:'/pages/charging/charging'}) }
 })
